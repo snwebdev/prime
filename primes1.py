487dabea67d92845e9e43c6c0f6a38cc38ae5453
Stewart <stewartnairn@live.com>
2020-05-24 22:42:01 +0100

    blah

--- primes1.py
+++ primes1.py
@@ -1,12 +1,14 @@
 def f(n):
+    primes = []
     d = 2
     for c in range(n):
         while d < c and not c % d == 0:
             d += 1
             if d == c:
-                print(c)
+                primes.append(c)
         d = 2
         c += 1
+    return(primes)
 
 
 print(f(200))
