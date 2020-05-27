6022eca57e8ac845dbc1f2f920fe7ccf18ba38ef
Stewart <stewartnairn@live.com>
2020-05-27 22:01:18 +0100

    added primes1

--- primes1.py
+++ primes1.py
@@ -1,12 +1,14 @@
-def f(n):
+def primes(n):
+    p = []
     d = 2
     for c in range(n):
         while d < c and not c % d == 0:
             d += 1
             if d == c:
-                print(c)
+                p.append(c)
         d = 2
         c += 1
+    return (p)
 
+print (primes(21))
 
-print(f(200))
